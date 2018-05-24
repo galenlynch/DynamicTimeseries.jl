b2e4499a2e1f37098d080219f3c68249c0e92ef3
Galen Lynch <galen.lynch@gmail.com>
2018-05-23 22:02:05 -0400

    Add comments to spectrogram downsamp_req

--- src/spectrogram.jl
+++ src/spectrogram.jl
@@ -39,21 +39,41 @@ function extrema(d::DynamicSpectrogram)
     return (freqs[1], freqs[end])
 end
 
+"""
+    downsamp_req(ds, xb, xe, npt; windowfun)
+
+Calculates the spectrogram for data in the range between xb and xe. Expands the
+selected range so the center of the first time bin is around xb, and the
+center of the last time bin is around xe.
+"""
 function downsamp_req(
     ds::DynamicSpectrogram, xb, xe, npt::Integer, args...;
     windowfun::Function = hanning
 )
-    # test
+    # Figure out what was asked of us
     nin = length(ds.input)
     (ib, ie) = clip_ndx.(t_to_ndx.([xb, xe], ds.fs, ds.offset), nin)
     win_l = length(ds.window)
     nsel = n_ndx(ib, ie)
     noverlap = floor(Int, ds.overlap * win_l)
+
+    # let:
+    # N := number of points in the selected region
+    # l := number of points in a window
+    # w := number of windows
+    # d := number of overlapping samples
+    # Then we have N = (w - 1) * l - (w - 1) * d
+    # => w = (N + l - d) / (l - d)
     win_nbase = div(nin + win_l - noverlap, win_l - noverlap)
+
     # Check if we have too many windows
     if win_nbase > npt
         # Too many windows: increase their size
-        win_l_target = cld(nsel, npt - floor(Int, ds.overlap * (npt - 1) - 1))
+
+        # f := fraction of window to overlap (=> d = fl)
+        # Substitute d = fl into the above, yielding:
+        # l = N / (w - f * (w - 1) - 1)
+        win_l_target = cld(nsel, npt - floor(Int, ds.overlap * (npt - 1)) - 1)
         was_downsamped = true
     else
         win_l_target = win_l
@@ -71,21 +91,24 @@ function downsamp_req(
 
     # Check if our window function is the right size
     if length(ds.window) != win_l_final
-        window = hanning # Pass the function itself
+        window = windowfun # Pass the function itself
     else
         window = ds.window
     end
 
-    # Expand selected data for edge bins
+    # Expand selected data for the bins on the edge
     half_w = div(win_l_final, 2)
     ib_ex = max(1, ib - half_w)
     ie_ex = min(nin, ie + half_w)
+
     # Select the portion of the signal in range
     sel_sig = ds.input[ib_ex:ie_ex]
+
     S = spectrogram(
         sel_sig, win_l_final, npt_overlap_final;
         fs = ds.fs, window = window
     )
+
     first_x = ndx_to_t(ib_ex, ds.fs, ds.offset)
     times = S.time + first_x
 
